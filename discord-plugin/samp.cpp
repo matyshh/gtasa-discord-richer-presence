2a50fd310b3a9efbd6e8120929da86a377fbb55b
Misa Stefanovic <40996107+MikMikYT@users.noreply.github.com>
2021-12-18 02:07:28 +0100

    fix samp crash, doesn't update in samp tho

--- discord-plugin/samp.cpp
+++ discord-plugin/samp.cpp
@@ -13,7 +13,7 @@ bool Samp::Init()
 	if (dwSAMP_Addr == NULL) dwSAMP_Addr = (DWORD)GetModuleHandleA("samp.dll");
 	if (dwSAMP_Addr == NULL) return false;
 
-	if (SAMP == nullptr) SAMP = *(stSAMP**)(dwSAMP_Addr + 0x21A0F8);
+	if (SAMP == nullptr) SAMP = *(stSAMP**)(dwSAMP_Addr + 0x215B40);
 	if (SAMP == nullptr) return false;
 
 	if (SAMP->iGameState != 14) return false;
